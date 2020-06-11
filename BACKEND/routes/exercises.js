3e32630499266a274fdb85de7511bfdb32b21bae
lucas <lcs999999999@hotmail.com>
2020-06-11 15:24:57 -0300

    username mistype solved

--- BACKEND/routes/exercises.js
+++ BACKEND/routes/exercises.js
@@ -8,7 +8,7 @@ router.route('/').get((req, res) => {
 });
 
 router.route('/add').post((req, res) => {
-    const username = res.body.username;
+    const username = req.body.username;
     const description = req.body.description;
     const duration = Number(req.body.duration);
     const date = Date.parse(req.body.date);
