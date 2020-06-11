5261f77cb602d84d682a8aecd41540b9eef73ae7
lucas <lcs999999999@hotmail.com>
2020-06-11 15:02:30 -0300

    backend

---
+++ BACKEND/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users')
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+const connection = mongoose.connection;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(uri,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+connection.once('open', () => {
+    console.log("\x1b[32m", "MongoDB database connection established successfully");
+});
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+app.listen(port,() => {
+    console.log("\x1b[36m", `Server is running on port ${port}`);
+});
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# MERN
+
+Mongo, Express, React and NodeJS personal study
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
