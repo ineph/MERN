e950c74e615b0069dcb076799cca92112b3b4164
lucas <lcs999999999@hotmail.com>
2020-06-15 18:53:53 -0300

    front end

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditExercises extends Component {
+    render() {
+        return(
+            <div>
+                <p>tou are in the edit EditExercises component</p>
+            </div>
+        );
+    };
+};
