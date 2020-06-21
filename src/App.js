259649a16b1cc8469cc3e7c3197d43c63eaac73a
lucas <lcs999999999@hotmail.com>
2020-06-21 16:46:00 -0300

    custom

--- src/App.js
+++ src/App.js
@@ -8,8 +8,16 @@ import EditExercise from "./components/edit-exercise.component";
 import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
 
+const darkBackgroud = {
+
+  // color: 'bisque',
+  // backgroundColor: '#202429',
+
+};
+
 function App() {
   return (
+    <div style={darkBackgroud}>
       <Router>
         <Navbar />
         <br/>
@@ -20,6 +28,7 @@ function App() {
         <Route path="/user" component={CreateUser} />
         </div>
       </Router>
+    </div>
   );
 }
 
--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css"
 
 export default class CreateExercise extends Component {
+
     constructor(props) {
         super(props);
 
@@ -17,6 +21,18 @@ export default class CreateExercise extends Component {
             date: new Date(),
             users: []
         }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/users')
+          .then(response => {
+            if (response.data.length > 0){
+              this.setState({
+                users: response.data.map(user => user.username),
+                username: response.data[0].username
+              });
+            }
+          });
     }
 
     onChangeUsername(e) {
@@ -53,15 +69,67 @@ export default class CreateExercise extends Component {
             date: this.state.date
         }
 
+        window.location = '/';
         console.log(exercise);
 
-        window.location = '/';
+        axios.post('http://localhost:5000/exercises/add', exercise)
+          .then(res => console.log(res.data));
     }
 
     render() {
         return(
-            <div>
-                <p>you are in the Create Exercise Component</p>
+            <div >
+              <h3>Create New Exercise Log</h3>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group"> 
+                  <label>Username: </label>
+                  <select ref="userInput"
+                      required
+                      className="form-control"
+                      value={this.state.username}
+                      onChange={this.onChangeUsername}>
+                      {
+                        this.state.users.map(function(user) {
+                          return <option 
+                            key={user}
+                            value={user}>{user}
+                            </option>;
+                        })
+                      }
+                  </select>
+                </div>
+                <div className="form-group"> 
+                  <label>Description: </label>
+                  <input  type="text"
+                      required
+                      className="form-control"
+                      value={this.state.description}
+                      onChange={this.onChangeDescription}
+                      />
+                </div>
+                <div className="form-group">
+                  <label>Duration (in minutes): </label>
+                  <input 
+                      type="text" 
+                      className="form-control"
+                      value={this.state.duration}
+                      onChange={this.onChangeDuration}
+                      />
+                </div>
+                <div className="form-group">
+                  <label>Date: </label>
+                  <div>
+                    <DatePicker
+                      selected={this.state.date}
+                      onChange={this.onChangeDate}
+                    />
+                  </div>
+                </div>
+        
+                <div className="form-group">
+                  <input type="submit" value="Create Exercise Log" className="btn btn-primary" />
+                </div>
+              </form>
             </div>
         );
     }
